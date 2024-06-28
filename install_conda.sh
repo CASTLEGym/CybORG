4624bf70a3563b96b0c968de639436fcd9d8af20
vardhah <harsh.vardhan@vanderbilt.edu>
2024-06-28 17:24:28 -0500

    cleaning up

--- install_conda.sh
+++ install_conda.sh
@@ -16,24 +16,7 @@ conda init
 rm -rf ~/miniconda.sh 
 # Verify installation
 conda --version
-
-
-
 conda create --name test_env python=3.10 -y
-conda activate test_env
-conda install -y pytorch  pytorch-cuda=11.8 -c pytorch -c nvidia
 
-#need to install git
-sudo apt-get install git
 
-git clone -b wrappers https://github.com/CASTLEGym/CybORG.git
 
-cd CybORG/CybORG/
-
-pip install openstackclient
-pip install pyvelociraptor
-conda install -c conda-forge grpcio -y
-pip install -e .
-
-cd CybORG/cage-2-cardiff
-python integrated_game_coordinator.py 
---
+++ install_test_run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+conda install -y pytorch  pytorch-cuda=11.8 -c pytorch -c nvidia
+
+#need to install git
+sudo apt-get install git
+
+git clone -b wrappers https://github.com/CASTLEGym/CybORG.git
+
+cd RAMPART_CybORG_EMU/CybORG/CybORG/
+
+pip install openstackclient
+pip install pyvelociraptor
+conda install -c conda-forge grpcio -y
+pip install -e .
+
+cd CybORG/cage-2-cardiff
+python integrated_game_coordinator.py 
