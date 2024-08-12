d97833fcc348d9a8f7b5d2fc628d1f9e9dc386bd
castledevvu <castledevvu@gmail.com>
2024-08-12 19:55:59 +0000

    started working in Monitor action

---
+++ RAMPART_CybORG_EMU/CybORG/CybORG/Emulator/Actions/Velociraptor/MonitorAction.py
@@ -0,0 +1,40 @@
+from typing import Union
+
+from xml.etree import ElementTree
+import time
+from .RunProcessAction import RunProcessAction
+from CybORG.Shared import Observation
+from CybORG.Simulator.State import State
+#from ...Observations.Velociraptor.MonitorObservation import MonitorObservation
+from CybORG.Emulator.Observations.Velociraptor.MonitorObservation import MonitorObservation
+
+class MonitorAction(RunProcessAction):
+
+    def __init__(self, credentials_file, hostname):
+        self.last_timestamp= int(time.time())
+        print("Current Unix time:", self.last_timestamp)
+        
+        # Define the path to your Zeek log file
+        self.LOG_FILE = "/usr/local/zeek/logs/current/conn.log"
+
+        super().__init__(
+            credentials_file=credentials_file,
+            hostname=hostname, 
+            command = f"cat {self.LOG_FILE}"
+            )
+
+
+
+    def execute(self, state: Union[State, None]) -> Observation:
+
+        observation = super().execute(state)
+        self.last_timestamp= int(time.time())
+
+        print('observation dict:',observation.__dict__)
+        print('Stdout is:',observation.Stdout)
+        if observation.success== True:
+          observation.set_success(True)
+          return MonitorObservation(observation,Observation.Stdout)
+        
+        observation.set_success(False)
+        
---
+++ RAMPART_CybORG_EMU/CybORG/CybORG/Tests/Actions/TestMonitor.py
@@ -0,0 +1,14 @@
+from CybORG.Emulator.Actions.Velociraptor.MonitorAction import MonitorAction
+
+
+credentials_file= "/home/ubuntu/prog_client.yaml"
+
+monitor_action = MonitorAction(
+    credentials_file=credentials_file,
+    hostname='monitoring-node'
+)
+
+observation = monitor_action.execute(None)
+print('success is:',observation.success)
+print('IP Address list is:',observation.connection_info)
+print("foo")
