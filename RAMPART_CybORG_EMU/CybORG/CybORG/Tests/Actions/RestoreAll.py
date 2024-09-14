9012024f05e8d06b976e3fd1cd7a161fca239367
Harmon Nine <Harmon.Nine@gmail.com>
2024-09-13 21:05:13 -0500

    Mods to make RestoreAll.py work on defender

--- RAMPART_CybORG_EMU/CybORG/CybORG/Tests/Actions/RestoreAll.py
+++ RAMPART_CybORG_EMU/CybORG/CybORG/Tests/Actions/RestoreAll.py
@@ -1,12 +1,18 @@
-from CybORG.Emulator.Actions.RestoreAction import RestoreAction
 import argparse
 import getpass
+from pathlib import Path
+import sys
+
+cyborg_dir = Path(Path(__file__).parent.parent.parent.parent).absolute()
+print(cyborg_dir)
+sys.path.append(str(cyborg_dir))
+
+from CybORG.Emulator.Actions.RestoreAction import RestoreAction
 
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-u", "--user", type=str, default="dummy", help="The user name for openstack (default:'dummy')")
-
     
 parser.add_argument( "-url",type=str,default="https://cloud.isislab.vanderbilt.edu:5000/v3", help="The url for openstack (dafault: Vanderbilt's opensctack cluster")
 parser.add_argument("-udn",type=str,default="ISIS", help="The user domain name for openstack (default: 'ISIS')")
@@ -33,6 +39,13 @@ vms=["user0","user1","user2","user3","user4","enterprise0","enterprise1","enterp
 for vm in  vms:
   print(f"resetting VM: {vm} .... ")
 
+  print(f"hostname = \"{vm}\"")
+  print(f"auth_url = \"{os_url}\"")
+  print(f"project_name = \"{project_name}\"")
+  print(f"username = \"{user_name}\"")
+  print(f"user_domain_name = \"{os_udn}\"")
+  print(f"project_domain_name = \"{os_pdn}\"")
+  print()
   restore_action = RestoreAction(
     hostname=vm,
     auth_url=os_url,
